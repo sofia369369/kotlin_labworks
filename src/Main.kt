0dcadb160f3d93b127aaf38589fcfc8fbad079b7
SofiaKh <skamenskih2007@gmail.com>
2026-05-04 21:11:33 +0300

    git remote add origin https://github.com/sofia369369/kotlin_labworks.git git branch -M main git push -u origin main

--- src/Main.kt
+++ src/Main.kt
@@ -3,7 +3,7 @@ fun main() {
     print("Привет! Я проверил, что Git и GitHub работают.")
 
     val a = 5
-    val b = 8
+    val b = 5
     val sum = a + b
     println("Сумма $a и $b равна $sum")
 }
